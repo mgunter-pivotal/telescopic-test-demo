150a9f64ebaab060b99beeb7ce10c1f4ad3796e0
Matt Gunter <mattgunter@Matts-MacBook-Pro.local>
2021-05-19 17:42:47 -0400

    seamless garden deploy, then k apply record/play.yaml

--- garden-projects/gcr-secret.sh
+++ garden-projects/gcr-secret.sh
@@ -1,15 +1,5 @@
 kubectl create secret docker-registry gcr-json-key \
 --docker-server=gcr.io \
 --docker-username=_json_key \
---docker-password="$({
-  "type": "service_account",
-  "project_id": "th-structure-flow-demo",
-  "private_key_id": "07c813c4a905bd9e5388eaea14ce91f33590af8b",
-  "client_email": "1084572173225-compute@developer.gserviceaccount.com",
-  "client_id": "100387121239928010552",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/1084572173225-compute%40developer.gserviceaccount.com"
-})" \
+--docker-password="$(cat ~/Downloads/th-structure-flow-demo-7aa8399569cb.json)" \
 --docker-email=mgunter@gmail.com
